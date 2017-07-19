a40e9507ee745dcf06b67c1370395972f7517481
Suraj Upreti <supreti@supreti-macbookpro.roam.corp.google.com>
2017-07-19 12:58:05 -0700

    implemented changing background color when button is clicked

--- app/src/main/java/com/example/supreti/lab2/MainActivity.java
+++ app/src/main/java/com/example/supreti/lab2/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -15,12 +16,14 @@ public class MainActivity extends AppCompatActivity {
     private Button mBlueButton;
     private Button mGreenButton;
 
-
+    private LinearLayout mLinearlayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mLinearlayout = (LinearLayout)findViewById(R.id.placeholder);
 
         mGeneralTextView = (TextView) findViewById(R.id.general_text_view);
         mBlueButton = (Button) findViewById(R.id.blue_button);
@@ -30,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 mGeneralTextView.setText(colorClick("blue"));
+                mLinearlayout.setBackgroundResource(R.color.holo_blue_bright);
                 Toast.makeText(MainActivity.this, colorClick("blue"), Toast.LENGTH_SHORT).show();
             }
         });
@@ -39,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 mGeneralTextView.setText(colorClick("green"));
+                mLinearlayout.setBackgroundResource(R.color.holo_green_light);
                 Toast.makeText(MainActivity.this, colorClick("green"), Toast.LENGTH_SHORT).show();
             }
         });
